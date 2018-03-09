230136b5cc0cd70a5fe1d6d907bf0403cce3050c
Kalen Davison <davison@wustl.edu>
2018-03-09 12:57:01 -0600

    Made a probability function.
    May return to this later and try to use apply instead of a for loop.

---
+++ Development.R
@@ -0,0 +1,34 @@
+### Set up packages
+rm(list=ls())
+library(devtools)
+library(roxygen2)
+
+#Function development
+
+testSubject = new("Rasch", name = "Benny", difficulty = 1:10, answers = sample(0:1, 10, replace=TRUE))
+testSubject
+
+Probability = function(raschObj, theta){
+  P = vector("numeric", length(raschObj@answers))
+  PQ = vector("numeric", length(raschObj@answers))
+  
+  for (i in 1:length(raschObj@answers)){
+    prob = (exp(theta-raschObj@difficulty[i]))/(1+(theta-raschObj@difficulty[i]))
+    P[i] = prob
+  }
+  
+  for (i in 1:length(raschObj@answers)){
+    if (raschObj@answers[i] == 1){
+      right = (exp(theta-raschObj@difficulty[i]))/(1+(theta-raschObj@difficulty[i]))
+      PQ[i] = right
+    }
+    else {
+      wrong = 1-(exp(theta-raschObj@difficulty[i]))/(1+(theta-raschObj@difficulty[i]))
+      PQ[i] = wrong
+    }
+  }
+  return(list(P, PQ))
+}
+
+Probability(raschObj = thing, theta = .5)
+
