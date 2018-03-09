fa1ee887bce3398187ed9e433edb08863450b01d
Kalen Davison <davison@wustl.edu>
2018-03-09 14:33:27 -0600

    Made a Prior R file in S4 package structure

---
+++ Master/easyRasch/R/Prior.R
@@ -0,0 +1,29 @@
+#' Rasch Model Prior
+#'
+#' Calculates the height of the normal curve given a theta value
+#'
+#' @param theta A numeric object 
+#' 
+#' @return A numeric
+#'  \item{height}{The height of the normal curve at a proposed theta}
+#' @author Kalen Davison: \email{davison@wustl.edu}
+#' @note This is a very simple function
+#' @examples
+#' 
+#' theta = 1.2
+#' Prior(theta)
+#' @seealso \code{\link{Likelihood}}
+#' @rdname Prior
+#' @aliases Print
+#' @export
+setGeneric("Prior", 
+           function(theta="numeric") {
+             standardGeneric("Prior")
+           } )
+
+setMethod(f = "Prior",
+          definition = function(theta){
+            height = dnorm(x = theta, mean = 0, sd = 3)
+            return(height)
+          }
+            )
